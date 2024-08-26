223fc8893b41775443a9fa02c356d0ac302c2f65
erickJzzCTL <ztl.erick@gmail.com>
2024-08-26 17:09:05 +0530

    erick home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,12 @@
+import SectionOne from '@/components/shared/homeScreens/SectionOne';
+import SubHeader from '@/components/shared/SubHeader';
 import React from 'react';
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <SubHeader />
+      <SectionOne />
+    </div>
+  );
 }
