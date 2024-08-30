cb13542df6bca825e05d26ea8613d0c4a3daa64e
a-s-aromal <aromalajithas@gmail.com>
2024-08-30 16:55:46 +0530

    changes

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -7,6 +7,8 @@ import { usePathname } from "next/navigation";
 import useStore from "@/store/store";
 import SignInPopup from "./popups/SignInPopup";
 import userValidate from "@/utilities/userValidate";
+import { useSetHomeData } from "@/hooks/useHomeData";
+import { AxiosResponse } from "axios";
 const whatsappsvg = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
